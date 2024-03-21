24b551be4a6802c41259e6b132bf5b034f44b58d
Cyril El Hakim <elhakim.cyril@outlook.com>
2024-03-21 17:14:40 +0100

    dans l'application app.js Middleware pour traiter les données JSON dans les requêtes et pour gérer les CORS en ajoutant  des headers (méthode setHeader) à notre objet  response

--- backend/app.js
+++ backend/app.js
@@ -5,5 +5,32 @@ const express = require('express');
 const app = express();
 
 
+// Middleware pour traiter les données JSON dans les requêtes
+app.use(express.json());
+
+// Middleware pour gérer les CORS (Cross-Origin Resource Sharing) --> système de sécurité qui empêche requêtes malveillantes d'accéder à des ressources sensibles
+//Pour cela, nous devons ajouter des headers sur objet res réponse (méthode setHeader) à notre objet  response  qu’on renvoie au navigateur 
+// pour lui dire TOUT VA BIEN : vous pouvez utiliser cette API
+app.use((req, res, next) => {
+	// On dit que l'Origin qui a le droit d'accéder à notre API c'est tout le monde d'où '*'
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	// Autorise certains en-têtes mentionnés aux requêtes envoyées vers notre API
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+	);
+	// On autorise certains types de méthodes (verbes HTTP) 
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+	);
+	//TOUT CA VA PERMETTRE à l’app d’accéder sans problèmes à l’API
+    // Ne pas oublier next pour passer l’éxécution au middleware d’après
+	next();
+});
+
+
+
+
 // On va exporter cette application Express pour qu'on puisse y accéder depuis les autres fichiers de notre projet et notamment notre serveur node
 module.exports = app;
