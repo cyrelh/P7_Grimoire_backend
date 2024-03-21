e3ecb802ebec27e61ac3213f034e4649b03dcf52
Cyril El Hakim <elhakim.cyril@outlook.com>
2024-03-21 23:34:47 +0100

    Réalisation de la fonction signup qui va crypter notre mdp Qui va prendre  ce mdp crypté et créer un nouveau user avec ce mdp crypté + email passé dans le corp de la req Et va enregistrer cet utilisateur dans la db On a donc notre logique de signup

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,32 @@
+//package de cryptage pour les mdp
+const bcrypt = require('bcrypt'); 
 
 // Import du modèle userSchema car on va enregistrer et lire des users dans les 2 middleware signup et login
 const userSchema = require('../models/userSchema');
 
 // Fonction logique métier signup pour l'inscription utilisateur
 exports.signup = (req, res, next) => {
+    	// Appel de la fonction pour hasher mdp
+	bcrypt.hash(req.body.password, 10) // on lui passe mdp corps de la requête qui sera passée par le frontend
+    //salt :nb de fois qu'on execute de l'algo de hashing --> ici 10 tours suffit pour mdp secured (+tours algo + tps)
+    // Méthode async pour recup le hash de mdp puis l'enregistrer dans un nouveau user crée danss la db
+    .then((hash) => {
+        // Création du nouvel utilisateur avec notre modèle mongoose User
+        const user = new User({
+            // Adresse mail fournie dans corps de la requête
+            email: req.body.email,
+            // on va enregistrer le hash en argument audessus pour pas stocker un mdp en blanc
+            password: hash
+        });
+        // Enregistrement du User dans la db
+        user.save()
+            // Si l'enregistrement est réussi on renvoie un code 201(= ressource crée) et un message
+            .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+            // Si l'enregistrement à échoué on renvoie une erreur 400 avec l'obj error pour différencier du catch en bas
+            .catch((error) => res.status(400).json({ error }));
+    })
+    // Erreur serveur
+    .catch((error) => res.status(500).json({ error }));
 
 };
 
