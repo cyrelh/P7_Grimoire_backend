0041d9a34a2bee90e41b52f6307f7966e4d05063
Cyril El Hakim <elhakim.cyril@outlook.com>
2024-03-22 12:14:35 +0100

    Exportation dans un fichier unique sharp-config.js du bookController.js pour le module Sharp pour le traitement des images

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -1,10 +1,8 @@
 const Book = require('../models/bookSchema');
-const fs = require('fs');
-const sharp = require('sharp');
 
-// Création d'un nouveau livre, le format sera différent car un fichier a été transmis
+// Création ou ajout d'un nouveau livre
 exports.createBook = (req, res, next) => {
-	//1- On va parser l’objet requête car objet envoyé dans la req va nous être envoyé sous forme JSON mais en string avec la fonction JSON.parse()
+	//1- On extrait champ  book de la req reçu en string pour convertir JSON en objet
 	const bookObject = JSON.parse(req.body.book);
 	// On va supprimer dans cet objet 2 champs --> '_id' et '_userId' de l'objet book
 	delete bookObject._id; // car l'id de l'obj va etre générer auto par notre db
@@ -13,46 +11,24 @@ exports.createBook = (req, res, next) => {
     // mais en nous envoyant le token id de qqun d'autre cer qui pourrait nous faire croire c'est cette autre personne qui a crée l'obj
 	
     
-    // On donne un nom de fichier pour l'img redimensionnée avec l'extension '.webp'
-	const resizedFileName = `resized-${req.file.filename.replace(/\.[^.]+$/,'')}.webp`;
-	// On crée le chemin d'accès pour l'img redimensionnée dans le dossier 'images'
-	const resizedImagePath = `./images/${resizedFileName}`;
-	// on utilise le package Sharp pour redimensionner l'img
-	sharp(req.file.path)
-		.resize(206, 260) // on redimensionne l'img à une largeur de 206 pixels et une hauteur de 260 pixels
-		.toFormat('webp', { quality: 80 }) // on convertit l'img en format WebP
-		.toFile(resizedImagePath, (err, info) => {// on gère les erreurs lors du redimensionnement et de la conversion
-			if (err) {
-				return res.status(401).json({ error: err.message });
-			}
-			// On supprime le fichier original après redimensionnement 
-            //grâce à méthode unlink() du package fs qui permet de supprimer un fichier du système de fichiers
-			fs.unlink(req.file.path, (unlinkErr) => {
-				if (unlinkErr) { // on gère les erreurs lors de la suppression du fichier original
-					console.error(
-						'Erreur lors de la suppression du fichier original:',
-						unlinkErr);
-				}
-				//2- On crée un objet Book avec l'URL de l'image redimensionnée
-				const book = new Book({
-					...bookObject, // avec ce qui a été passé en 1- moins les _userId et _id
+	//2- On crée nouvelle instance du modèle booskSchema -combine les propriétés de bookObject avec l'ID de l'utilisateur et l'URL de l'image
+	const book = new Book({
+	...bookObject, // avec ce qui a été passé en 1- moins les _userId et _id
+	userId: req.auth.userId, // les userId On les extrait de l'obj req grâce à notre middleware
+    // et on va générer l'URL de l'img MAIS mais multer ne ns passe que le nom du fichier
+    // Donc on fait appel à des propriétés de l'obj req (le protocol, le nom d'hote / images là où les stocke et nom de fichier redim )
+    imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`,    });
 
-					userId: req.auth.userId, // les userId On les extrait de l'obj req grâce à notre middleware
-                    // et on va générer l'URL de l'img MAIS mais multer ne ns passe que le nom du fichier
-                    // Donc on fait appel à des propriétés de l'obj req (le protocol, le nom d'hote / images là où les stocke et nom de fichier redim )
-					imageUrl: `${req.protocol}://${req.get('host')}/images/${resizedFileName}`});
+    book.save() // On enregistre ce fichier book dans la base de données avec méthode save()
 
-				// On enregistre ce fichier book dans la base de données avec méthode save()
-				book.save()
-					.then(() => {
-						res.status(201).json({ message: 'Livre enregistré !' });
-					})
-					.catch((error) => {
-						res.status(404).json({ error: "Erreur lors de l'enregistrement !" });
-					});// si erreur 404, c'est lié au fait que la req vers notre répertoire images n'est pas gérée
-                    // car c'est notre serveur qui gère absoluement toutes les req
-                    // donc il nous faut ajouter un route pour géréer ça dans app.js 
-                    //app.use('/images', express.static(path.join(__dirname, 'images')));
-			});
-		});
-};
+    .then(() => {
+        res.status(201).json({ message: 'Livre enregistré !' });
+    })
+    .catch((error) => {
+        res.status(400).json({ error: "Erreur lors de l'enregistrement !" });
+    });
+};// si erreur 404, c'est lié au fait que la req vers notre répertoire images n'est pas gérée
+// car c'est notre serveur qui gère absoluement toutes les req
+// donc il nous faut ajouter un route pour géréer ça dans app.js 
+//app.use('/images', express.static(path.join(__dirname, 'images')));
+    
---
+++ backend/middleware/sharp-config.js
@@ -0,0 +1,44 @@
+const sharp = require("sharp"); // Importe le module Sharp pour le traitement des images
+const fs = require("fs"); // Importe le module FileSystem pour la manipulation des fichiers
+
+const imageConversion = (req, res, next) => { // Définit une fonction middleware pour la conversion des images
+  // Vérifie si un fichier est téléchargé
+  if (!req.file) { // Si aucun fichier n'est téléchargé dans la requête, passe à la prochaine fonction middleware
+    return next();
+  }
+
+  console.log("Nom de fichier d'origine :", req.file.filename);
+
+  // on met le nom de fichier à jour avant utilisation de sharp
+  req.file.filename = req.file.filename.replace(/(.*)(\.jpg|\.jpeg|\.png)(?=[^.]*$)/, "$1.webp"); // Définit le chemin du fichier redimensionné avec l'extension .webp
+  console.log("Nom de fichier mis à jour :", req.file.filename); // affiche dans la console le nom de fichier d'origine de la requête
+
+  // on crée le chemin complet où le fichier redimensionné sera enregistré avec l'extension.webp
+  const newFilePath = `images/${req.file.filename}`;
+  console.log("Chemin du fichier pour Sharp :", newFilePath); 
+  //affiche chemin complet où le fichier redimensionné sera enregistré avec l'extension .webp
+
+  sharp(req.file.path) 	// on redimensionne l'image à une largeur de 206 pixels et une hauteur de 260 pixels
+    .resize({
+      width: 206,
+      height: 260,
+      fit: "cover",
+    })
+    .toFormat("webp", { quality: 90 })  // on converit l'image en format WebP
+    .toFile(newFilePath) // Utilise le nouveau chemin de fichier
+    .then(() => {
+      // Supprime le fichier d'origine du système de fichier après la conversion en WebP
+      fs.unlink(req.file.path, (err) => {
+        if (err) {// on gère les erreurs lors du redimensionnement et de la conversion
+          console.error("Erreur lors de la suppression du fichier original:", err); 
+        }
+        next();
+      });
+    })
+    .catch((error) => {
+      console.error("Erreur de traitement d'image :", error); // affiche dans la console une erreur survenue lors du traitement de l'image
+      next();
+    });
+};
+
+module.exports = imageConversion; // Exporte la fonction middleware imageConversion
